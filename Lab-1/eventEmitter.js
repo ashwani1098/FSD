e53874a27ded8381ea27041266b7bd849c34579e
ashwani1098 <vikassingh95656435@gmail.com>
2026-07-17 15:42:32 +0530

    lab-1

---
+++ Lab-1/eventEmitter.js
@@ -0,0 +1,38 @@
+import { log } from "node:console";
+import{EventEmitter} from "node:events";
+
+const task=new EventEmitter();
+
+const sayHi=(name)=>{
+    console.log(`Logged in ${name}`);   
+};
+
+task.on("greet",sayHi);
+
+task.on("greet",()=>{
+    console.log("Logged out")
+});
+
+
+task.once("greet",()=>{
+    console.log("System Started...")
+});
+
+task.on("greet",(name)=>{
+    console.log(`System is shutting down by ${name}`)
+});
+
+
+
+// const shutdown = (name) => {
+//     console.log(`System is shutting down by ${name}`);
+// };
+
+task.on("greet", shutdown);
+
+// task.off("greet", shutdown); 
+
+task.emit("greet","Akhil Singh");
+task.emit("greet","Sneha Singh");
+task.emit("greet","Rahul Singh");
+
---
+++ Lab-1/index.js
@@ -0,0 +1,16 @@
+// Write a fn to take your name and print it with hello msg
+function sayHello(name){
+    console.log(`Hello ${name}`);
+}
+const sayHi=(name)=>{
+    console.log(`Hi ${name}`);
+}
+sayHello("Anuj Sharma");
+sayHi("Manju Sinha");
+
+const findSum=(a,b)=>{
+   return a+b;
+};
+const sum=(a,b)=>a+b;
+console.log("sum=",findSum(5,7));
+console.log("sum=",sum(5,7));
